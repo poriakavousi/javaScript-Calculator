644bed1fd5a71f5d204cec01bc86f23fa432fb04
pegasus <poriakavousi@gmail.com>
2025-08-25 13:01:29 -0700

    change style & math symbol

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function addToCalucator(value){
     document.getElementById("displayResuly").value+=value;
 }
+function finilize(){
+     document.getElementById("displayResuly").value = eval( document.getElementById("displayResuly").value);
+}
+function reset(){
+     document.getElementById("displayResuly").value = " ";
+}
+function mathCalculator(mathFunc){
+    
+var val = document.getElementById("displayResuly").value;
+ document.getElementById("displayResuly").value = Math[mathFunc](val);
+}
